472155aaf7abea1678cc92b747a3e2d4eccc84d6
JaeYeon <kim802@purdue.edu>
2017-01-21 14:55:17 -0500

    updated on main(game) page
    you can create and join game and see the game list

--- app/src/main/java/com/bridge/hackathon/sportsbettingapp/Game.java
+++ app/src/main/java/com/bridge/hackathon/sportsbettingapp/Game.java
@@ -6,11 +6,72 @@ package com.bridge.hackathon.sportsbettingapp;
 
 public class Game {
     private int id;
-    private int time;
-    private String playerName1;
-    private String playerName2;
+    //profile image from faceobok
+    private String player1;
+    private String player2;
+    private int amount;
+
     private String teamName1;
     private String teamName2;
 
+    //private int record;
 
+    //constructor
+    public Game(int id, String player1, String player2, int amount){
+        this.id = id;
+        this.player1 = player1;
+        this.player2 = player2;
+        this.amount = amount;
+    }
+
+    // Setter and getter
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getPlayer1() {
+        return player1;
+    }
+
+    public void setPlayer1(String player1) {
+        this.player1 = player1;
+    }
+
+    public String getPlayer2() {
+        return player2;
+    }
+
+    public void setPlayer2(String player2) {
+        this.player2 = player2;
+    }
+
+    public String getTeamName1() {
+        return teamName1;
+    }
+
+    public void setTeamName1(String teamName1) {
+        this.teamName1 = teamName1;
+    }
+
+    public String getTeamName2() {
+        return teamName2;
+    }
+
+    public void setTeamName2(String teamName2) {
+        this.teamName2 = teamName2;
+    }
 }
