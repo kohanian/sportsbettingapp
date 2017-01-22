9829dda092a244e507cfdca6ccd6310936161d63
Bobby Gupta <gupta298@purdue.edu>
2017-01-21 20:24:15 -0500

    Add files via upload

---
+++ Manipulation.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+/**
+ * Created by Bobby Gupta on 1/21/2017.
+ */
+public class Manipulation extends Variables {
+    public void addParticipant (String email, int bet) {
+        participants.put(email, bet);
+        emails.add(email);
+        pair(email, bet);
+    }
+
+    public void pair (String email, int bet) {
+        if (participants.size() >= 2) {
+            int finalBet = 0;
+            int betPlayer1 = participants.get(emails.get(0));
+            int betPlayer2 = participants.get(emails.get(1));
+
+            if (betPlayer1 <= betPlayer2) {
+                finalBet = betPlayer1;
+            } else {
+                finalBet = betPlayer2;
+            }
+
+            while (true) {
+                try {
+                    bets.get(finalBet);
+                    finalBet++;
+                } catch (Exception e) {
+                    break;
+                }
+            }
+
+            bets.put(finalBet, new ArrayList<String>());
+            bets.get(finalBet).add(emails.get(0));
+            bets.get(finalBet).add(emails.get(1));
+            emails.remove(0);
+            emails.remove(0);
+            participants.remove(0);
+            participants.remove(0);
+        }
+    }
+}
---
+++ Variables.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by Bobby Gupta on 1/21/2017.
+ */
+public class Variables {
+    HashMap<String, Integer> participants = new HashMap<String, Integer>();
+    ArrayList<String> emails = new ArrayList<String>();
+    HashMap<Integer, ArrayList<String>> bets = new HashMap<Integer, ArrayList<String>>();
+}
